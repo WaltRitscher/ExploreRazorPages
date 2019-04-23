817af0bb6351213cecd94fce90888a9a01c13932
Walt Ritscher <waltritscher@outlook.com>
2019-04-23 06:50:07 -0700

    Add another model binding demo

--- ExploreRazorPages/Models/Customer.cs
+++ ExploreRazorPages/Models/Customer.cs
@@ -4,7 +4,7 @@ using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace ExploreRazorPages.Models
+namespace TailorMadeTours.Models
 {
 	public class Customer
 	{
@@ -19,5 +19,7 @@ namespace ExploreRazorPages.Models
 		public bool IsActive { get; set; }
 		[MinLength(12)]
 		public string Notes { get; set; }
+		
+		public String	 AdminNotes { get; set; }
 	}
 }
---
+++ ExploreRazorPages/Pages/Binding/ModelBindingExample.cshtml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using TailorMadeTours.Models;
+
+namespace ExploreRazorPages.Pages.Binding
+{
+	public class ModelBindingExampleModel : PageModel
+	{
+		// 	Model Binding in Razor Pages is the process that takes values from HTTP requests and maps them to handler method parameters or PageModel properties.
+		//  By default, only values that form part of a POST request are considered for model binding when you use the BindProperty attribute.
+		[BindProperty(SupportsGet =true)]
+		public Customer Customer { get; set; }
+		
+		public void OnGet([Bind] string FirstName, [Bind]bool IsActive)
+		{
+			// 
+
+			var currentCustomer = Customer;
+			
+		}
+	}
+}
